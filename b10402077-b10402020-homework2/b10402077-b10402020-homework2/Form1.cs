f2eb0c5c989c0663146a0ef7633c4deeb79317de
stevenlee1997 <changchinlee1997@gmail.com>
2017-11-24 11:29:19 +0800

    problem4

--- b10402077-b10402020-homework2/b10402077-b10402020-homework2/Form1.cs
+++ b10402077-b10402020-homework2/b10402077-b10402020-homework2/Form1.cs
@@ -16,5 +16,19 @@ namespace b10402077_b10402020_homework2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n;
+            comboBox1.Items.Clear();
+            Class1 random_num = new Class1();
+            Class2 prime_num = new Class2();
+            n = random_num.RandomNum();
+            label1.Text = "" + n;
+            for(int i=2;i<=n;i++)
+            {
+                if (prime_num.PrimeNum(i) == true) comboBox1.Items.Add(i);
+            }
+        }
     }
 }
