df8b00d3ce45e79e7bf4cccdb870115f986ac8d8
stevenlee1997 <changchinlee1997@gmail.com>
2017-11-24 11:00:49 +0800

    problem1
    建置專案

---
+++ b10402077-b10402020-homework2/b10402077-b10402020-homework2/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace b10402077_b10402020_homework2
+{
+    class Class1
+    {
+    }
+}
